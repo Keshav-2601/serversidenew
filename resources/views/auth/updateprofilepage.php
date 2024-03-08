21618dc09c5f3cad3c70496a8a228d1caffb8224
Keshav-2601 <keshavv857@gmail.com>
2024-03-08 20:45:04 +0000

    git 5 updations in view of updatePRofileview page

--- resources/views/auth/updateprofilepage.php
+++ resources/views/auth/updateprofilepage.php
@@ -1,25 +1,27 @@
-@include('layout.app')
-
 @section('content')
 <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
     <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
-        {{ __('UpdateProfile') }}
+        UpdateProfile
     </header>
     <form class="w-full px-6 space-y-6 sm:px-10 sm:space-y-8" method="POST" action="{{ route('login') }}">
-        @csrf
         <div class="flex flex-wrap">
             <label for="email" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-                {{ __('E-Mail Address') }}:
+                E-Mail Address:
             </label>
+            <input type="Email" id="email"/>
         </div>
         <div class="flex flex-wrap">
             <label for="password" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-                {{ __('Password') }}:
+                Password':
             </label>
+            <input type="password" id="password"/>
+        </div>
+        <div>
+            <button type="submit">SUBMIT</button>
         </div>
     </form>
 
 </section>
 @endsection
-@include('layout.footer');
+@extends('layout.footer');

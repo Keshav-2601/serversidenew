5f05d1eb9e835663d3dfedce9ace6add3aaab8de
Keshav-2601 <keshavv857@gmail.com>
2024-03-09 20:26:49 +0000

    Git 6 making changes in controller for update profile

--- app/Http/Controllers/Auth/Updateprofilecontroller.php
+++ app/Http/Controllers/Auth/Updateprofilecontroller.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-
-
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 class Updateprofilecontroller extends Controller{
 
-    public function update(){
-        return view("auth.updateprofilepage");
-    }
+   
+   public function  update(){
+    $User=Auth::user();
+    // dd($User);
+    $name=$User->name;
+    $email=$User->email;
+    $password=$User->password;
+    return view("auth.updateprofilepage",['name'=>$name,'email'=>$email,'Password'=>$password]);
+   }
+    
 }
--- resources/views/auth/updateprofilepage.php
+++ resources/views/auth/updateprofilepage.php
@@ -1,22 +1,29 @@
-@section('content');
+@section('content')
 <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
 
     <header class="font-semibold bg-gray-200 text-gray-700 py-5 px-6 sm:py-6 sm:px-8 sm:rounded-t-md">
         UpdateProfile
     </header>
-    <form class="w-full px-6 space-y-6 sm:px-10 sm:space-y-8" method="POST" action="{{ route('login') }}">
+    <form class="w-full px-6 space-y-6 sm:px-10 sm:space-y-8" method="POST" action="{{route('updateprofile')}}">
+        <div class="flex flex-wrap">
+            <label for="name" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                Name:
+            </label>
+            <input type="text" id="name" value="{{ $name }}" />
+        </div>
         <div class="flex flex-wrap">
             <label for="email" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
                 E-Mail Address:
             </label>
-            <input type="Email" id="email"/>
+            <input type="Email" id="email" value="{{ $email }}" />
         </div>
         <div class="flex flex-wrap">
-            <label for="password" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
-                Password':
+            <label for="Password" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                Password:
             </label>
-            <input type="password" id="password"/>
+            <input type="text" id="Password" value="{{ $password }}"/>
         </div>
+
         <div>
             <button type="submit">SUBMIT</button>
         </div>
@@ -24,4 +31,3 @@
 
 </section>
 @endsection
-@extends('layouts.footer');

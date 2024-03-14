06b26d5d5121309a15c376c0be62ea0a8c78e1da
Keshav-2601 <keshavv857@gmail.com>
2024-03-14 19:27:43 +0000

    git 28 completing of blog page css

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div class="w-4/5 m-auto text-center">
     <div class="py-15 border-b border-gray-200">
-        <h1 class="text-6xl">
+        <h1 class="blogtext">
             Blog Posts
         </h1>
     </div>
@@ -35,24 +35,24 @@
             <img src="{{ asset('images/' . $post->image_path) }}" alt="">
         </div>
         <div>
-            <h2 class="text-gray-700 font-bold text-5xl pb-4">
+            <h2 class="blogtitle">
                 {{ $post->title }}
             </h2>
 
-            <span class="text-gray-500">
-                By <span class="font-bold italic text-gray-800">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
+            <span class="blogusername">
+                By <span class="blogusername">{{ $post->user->name }}</span>, Created on {{ date('jS M Y', strtotime($post->updated_at)) }}
             </span>
 
-            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+            <p class="blogdescp">
                 {{ $post->description }}
             </p>
 
-            <a href="/blog/{{ $post->slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+            <a href="/blog/{{ $post->slug }}" class="Blogmainbutton">
                 Keep Reading
             </a>
            
             @if (isset(Auth::user()->id) && Auth::user()->id == $post->user_id)
-            <a href="{{route('writearticle',['id'=>$post->id])}}" class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">Write-Article</a>
+            <a href="{{route('writearticle',['id'=>$post->id])}}" class="Blogmainbutton">Write-Article</a>
 
                 <span class="blogbutton">
                     <a 

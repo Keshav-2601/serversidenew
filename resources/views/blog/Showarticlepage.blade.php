85a5cb2163007c77333616e17fdf80966e2d94c5
Keshav-2601 <keshavv857@gmail.com>
2024-03-15 16:22:21 +0000

    git 32 making changes in css showartpage file

--- resources/views/blog/Showarticlepage.blade.php
+++ resources/views/blog/Showarticlepage.blade.php
@@ -1,8 +1,7 @@
-
 @section('styles')
  <link href="{{ asset('css/showartpage.css') }}" rel="stylesheet">
 @endsection
 @foreach($articles as $article)
-<h2>{{$article->title}}</h2>
-<p>{{$article->content}}</p>
+<h2 class="titleheading">{{$article->title}}</h2>
+<p class="titlecontent">{{$article->content}}</p>
 @endforeach
